58005ba33720ea01b439a05baa974f8fc585ea49
프로용병 <untjrgus@naver.com>
2025-06-10 09:09:10 +0900

    Initial Commit

---
+++ src/main/java/kr/ac/kopo/ctc/kopo01/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ctc.kopo01.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
